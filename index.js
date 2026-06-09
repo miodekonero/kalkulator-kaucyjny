f117a496d59396e2a1a1b754c972a6c9e09e326f
miodekonero <163750087+miodekonero@users.noreply.github.com>
2026-06-09 11:41:24 +0200

    Main commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function update(element) {
+    let multiplier = container.value === "glass-bottle" ? 1.0 : 0.5;
+    if (element === amount) {
+        total.value = amount.value * multiplier
+    }
+    else {
+        amount.value = total.value * (1/multiplier)
+    }
+}
+
+function validateInput(event) {
+    if (["e", "-", "+"].includes(event.key)) { event.preventDefault() }
+}
+
+window.onload = () => {
+    const total = document.getElementById("total");
+    const amount = document.getElementById("amount");
+    const container = document.getElementById("container");
+
+    [total, amount].forEach((element) => { element.addEventListener("keypress", validateInput) })
+}
