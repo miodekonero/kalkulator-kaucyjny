35336f02fb5c2dab2c7e3153191d33c4b5e65488
miodekonero <@>
2026-06-09 22:57:56 +0200

    Reworked the calculation system to be more "smooth" and convenient to use

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-function update(element) {
-    let multiplier = container.value === "glass-bottle" ? 1.0 : 0.5;
-    if (total.value === "" && amount.value === "") {
-        return;
-    }
-    if (element === amount) {
-        total.value = amount.value * multiplier;
-    }
-    else {
-        amount.value = total.value * (1/multiplier);
-    }
-}
-
-function validateInput(event) {
-    if (["e", "-", "+"].includes(event.key)) { event.preventDefault() }
-}
-
+let last_updated_input = null;
 window.onload = () => {
     const total = document.getElementById("total");
     const amount = document.getElementById("amount");
     const container = document.getElementById("container");
 
-    [total, amount].forEach((element) => { element.addEventListener("keypress", validateInput) });
+    total.addEventListener("keypress", validateInput);
+    amount.addEventListener("keypress", validateInput);
+}
+
+function update(element) {
+    if (element !== container) {
+        let multiplier = container.value === "glass-bottle" ? 1.0 : 0.5;
+        last_updated_input = element;
+        if (element.value === "") {
+            amount.value = "";
+            total.value = "";
+        }
+        else if (element === amount) { total.value = amount.value * multiplier }
+        else if (element === total) { amount.value = total.value * (1/multiplier) }
+    }
+    else if (last_updated_input !== null) { update(last_updated_input) }
+}
+
+function validateInput(event) {
+    if (["e", "-", "+"].includes(event.key)) { event.preventDefault() }
 }
